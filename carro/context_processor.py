83f517fc1aea08eda666a7992fe834b5626222a3
Jorge-Agustin-Loyola <112104103+Jorge-Agustin-Loyola@users.noreply.github.com>
2024-04-06 19:01:15 -0300

    se modifico carro para que se muetre solo si estsas logeado

--- carro/context_processor.py
+++ carro/context_processor.py
@@ -1,8 +1,7 @@
 def importe_total_carro(request):
     total=0
     if request.user.is_authenticated:
-        if 'carro' is not request.session:
-            request.session['carro'] = {}
+        
 
         for key, value in request.session["carro"].items():
             
