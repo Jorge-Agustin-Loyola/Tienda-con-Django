933d5865829e192dcaddcf6b09b55105b902128d
Jorge-Agustin-Loyola <112104103+Jorge-Agustin-Loyola@users.noreply.github.com>
2024-04-06 17:11:29 -0300

    se le agrago mensaje de erro al login

--- ProyectoWeb/settings.py
+++ ProyectoWeb/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 
+from django.contrib.messages import constants as mensajes_de_error
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -147,3 +149,11 @@ EMAIL_HOST_PASSWORD = "ypkwcxymmzdhfajg"
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
+
+MESSAGE_TAGS={
+    mensajes_de_error.DEBUG: 'debug',
+    mensajes_de_error.INFO: 'info',
+    mensajes_de_error.SUCCESS: 'success',
+    mensajes_de_error.WARNING: 'warning',
+    mensajes_de_error.ERROR: 'danger',
+}
